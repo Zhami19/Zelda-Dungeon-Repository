026efb32f68f3804c3c9553cba0f6caaee89bce5
Zhamanta Chavez <zhamantach04@gmail.com>
2024-04-16 22:34:42 -0500

    Imported

---
+++ Assets/3DGamekitLite/Packages/KeySystem/DisableKey.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableKey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HideKey()
+    {
+        Key k = FindObjectOfType<Key>();
+        k.gameObject.SetActive(false);
+
+        Destroy(this);
+    }
+}
---
+++ Assets/3DGamekitLite/Packages/KeySystem/FollowPlayerHead.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gamekit3D
+{
+    public class FollowPlayerHead : MonoBehaviour
+    {
+        public Transform target;
+        public Vector3 offset;
+
+        int index = 0;
+
+        public void Awake()
+        {
+            offset = new Vector3(Random.Range(-.70f, .70f),
+                                 Random.Range(-.70f, .70f),
+                                 Random.Range(-.70f, .70f));
+
+            // future update: make keys spiral around player?
+
+        }
+
+        private void LateUpdate()
+        {
+            if(target == null)
+            {
+                target = GameObject.Find("HeadTarget").transform;
+            }
+            if (target == null)
+                return;
+
+            transform.position = target.position + offset;
+            transform.rotation = target.rotation;
+        }
+    } 
+}
